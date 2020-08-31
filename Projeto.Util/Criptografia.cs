61a4e25ab5609ba2868e80fbe1eeeb5748640878
Igor Araújo Nunes de Souza <54675811+IgorSouza1987@users.noreply.github.com>
2020-08-31 18:44:47 -0300

    Add files via upload

---
+++ Projeto.Util/Criptografia.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography; 
+
+
+namespace Projeto.Util
+{
+    public class Criptografia
+    {
+       
+        public string EncriptarMD5(string valor)
+        {
+            
+            MD5 md5 = new MD5CryptoServiceProvider();
+            
+            byte[] hash = md5.ComputeHash(Encoding.UTF8.GetBytes(valor));
+           
+            string result = string.Empty;
+            foreach (var item in hash)
+            {
+                result += item.ToString("X2"); 
+            }
+            return result;
+        }
+    }
+}
